054ab8c836806053f070042c991cc6108881e1c3
Magdaline <mamoakohene@gmail.com>
2022-11-21 01:42:01 +0000

    Takeaway project second draft

--- solo_project/lib/checkout.rb
+++ solo_project/lib/checkout.rb
@@ -21,8 +21,24 @@ class Checkout
   
     #def order_status
         #pay = true
-      # checks if order has been paid
-      # send text "Thank you! Your order was placed and will be delivered before 18:52"
+        # send text "Thank you! Your order was placed and will be delivered before 18:52"
     #end
 
 end
+
+=begin
+issues with Twilio, unable to create login for text.
+
+order_status
+        @client = Twilio::REST::Client.new
+        @client.messages.create
+
+require './order'
+
+test = Order.new({"Margherita" => 8, "Pepperoni" => 10, "Meat feast" => 12, "Veggie delight" => 10})
+test.add("Veggie delight")
+test_2 = Checkout.new(test)
+test_2.pay(25)
+p test_2.order_status
+
+=end
--- solo_project/spec/checkout_spec.rb
+++ solo_project/spec/checkout_spec.rb
@@ -2,40 +2,23 @@ require 'checkout'
 
 describe Checkout do
 
-    xit "allows user to checkout" do
-        fake_order = double :order, menu: [{"Margherita" => 8, "Pepperoni" => 10, "Meat feast" => 12, "Veggie delight" => 10}]
-        allow(fake_order).to receive(:add).with("Pepperoni").and_return nil
-        fake_order.add("Pepperoni")
-        checkout = Checkout.new(fake_order)
-        expect(checkout.pay(22)).to eq "Thank you! Your order was placed and will be delivered before 18:52"
-    end #expect is not pay, it should be order status!
-
     it "allows user to view an itemised receipt once paid" do
-        fake_order = double :order, menu: [{"Margherita" => 8, "Pepperoni" => 10, "Meat feast" => 12, "Veggie delight" => 10}]
-        allow(fake_order).to receive(:add).with("Pepperoni").and_return nil
-        allow(fake_order).to receive(:add).with("Meat feast").and_return nil
-        fake_order.add("Pepperoni")
-        fake_order.add("Meat feast")
+        fake_order = double :order, basket: [{"Margherita" => 8}, {"Pepperoni" => 10}, "Order total: 18"]
         checkout = Checkout.new(fake_order)
-        checkout.pay(22)
-        expect(checkout.print_receipt).to eq [{"Pepperoni" => 10}, {"Meat feast" => 12}, "Order total: 22"]
+        checkout.pay(18)
+        expect(checkout.print_receipt).to eq [{"Margherita" => 8}, {"Pepperoni" => 10}, "Order total: 18"]
     end
 
-    xcontext "fails" do
+    context "fails" do
         it "fails if payment is insufficient" do
-            fake_order = double :order
-            fake_order.add("Pepperoni")
-            fake_order.add("Meat feast")
+            fake_order = double :order, basket: [{"Margherita" => 8}, {"Pepperoni" => 10}, "Order total: 18"]
             checkout = Checkout.new(fake_order)
             expect{ checkout.pay(15) }.to raise_error ("Order failed. Please try again")
         end
 
         it "fails to print a recipt if order is not paid" do
-            fake_order = double :order
-            fake_order.add("Pepperoni")
-            fake_order.add("Meat feast")
+            fake_order = double :order, basket: [{"Margherita" => 8}, {"Pepperoni" => 10}, "Order total: 18"]
             checkout = Checkout.new(fake_order)
-            checkout.pay(15)
             expect{ checkout.print_receipt }.to raise_error ("Order failed. Please try again")
         end
     end
